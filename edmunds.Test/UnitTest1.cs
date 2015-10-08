bd7c228461c1e5ef6d015893671c602ced9fbdd9
alfredo aguirre <alfredodejesusaguirre@yahoo.com>
2015-10-07 22:43:52 -0500

    init commit

---
+++ edmunds.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace edmunds.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var class1 = new Class1();
+            class1.Invoke();
+        }
+    }
+}
