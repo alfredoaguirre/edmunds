8eb28c84cada5e38602ec67b2b6b873f1c7c6572
alfredo aguirre <alfredodejesusaguirre@yahoo.com>
2015-10-13 23:21:30 -0500

    get jsone and send get request to the edmunds service

--- NodejsConsoleApp13/app.js
+++ NodejsConsoleApp13/app.js
@@ -1,2 +1,24 @@
 ﻿console.log('Hello world');
 var http = require('http');
+
+//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+var options = {
+    host: 'api.edmunds.com',
+    path: '/api/vehicle/v2/Acura/Integra?fmt=json&api_key=67t7jtrnvz8wyzgfpwgcqa3y'
+};
+
+callback = function (response) {
+    var str = '';
+    
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+        str += chunk;
+    });
+    
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+        console.log(str);
+    });
+}
+
+http.request(options, callback).end();
--- NodejsWebApp1/index.js
+++ NodejsWebApp1/index.js
@@ -1,38 +1,70 @@
 // For development/testing purposes
 
 exports.handler = function( event, context ) {
-    console.log( "Running index.handler" );
+    console.log("Running index.handler");
+    var arg = event.request.intent.slots;
     console.log( "==================================");
-    console.log( "event", event );
+    console.log( "event", arg );
     console.log( "==================================");
     console.log("Stopping index.handler");
     var http = require('http');
+    this.context = context;
+    this.model = arg.model.value;
+    this.make = arg.make.value;
+    this.year = arg.year.value;
     
-    http://api.edmunds.com/api/vehicle/v2/{make}/{model}?fmt=json&api_key={67t7jtrnvz8wyzgfpwgcqa3y}
-    
-
-    var response = {
-        "version": "1.0",
-        "sessionAttributes": {},
-        "response": {
-            "outputSpeech": {
-                "type": "PlainText",
-                "text": "Welcome to the Alexa Skills Kit sample, Please tell me your favorite color by saying, my favorite color is red"
-            },
-            "card": {
-                "type": "Simple",
-                "title": "SessionSpeechlet - Welcome",
-                "content": "SessionSpeechlet - Welcome to the Alexa Skills Kit sample, Please tell me your favorite color by saying, my favorite color is red"
-            },
-            "reprompt": {
-                "outputSpeech": {
-                    "type": "PlainText",
-                    "text": "Please tell me your favorite color by saying, my favorite color is red"
-                }
-            },
-            "shouldEndSession": false
-        }
+    var options = {
+        host: 'api.edmunds.com',
+        path: '/api/vehicle/v2/' + make + '/' + model + '/'+ year + '/styles?view=full&fmt=json&api_key=67t7jtrnvz8wyzgfpwgcqa3y'
     };
-    context.succeed(response); 
+    
+    callback = function (response) {
+        var str = '';
+        
+        //another chunk of data has been recieved, so append it to `str`
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+        
+        //the whole response has been recieved, so we just print it out here
+        response.on('end', function () {
+            console.log(str);
+            var resp = JSON.parse(str);
+            var respString = "";
+            if (resp.styles[0])
+                respString = make + " " + model + " " + year + " highway: " + resp.styles[0].MPG.highway + " city: " + resp.styles[0].MPG.city;
+            else
+                respString = "No data for " + make + " " + model + " " + year;
+            var response = {
+                "version": "1.0",
+                "sessionAttributes": {},
+                "response": {
+                    "outputSpeech": {
+                        "type": "PlainText",
+                        "text": respString,
+                    },
+                    "card": {
+                        "type": "Simple",
+                        "title": "SessionSpeechlet - Welcome",
+                        "content": respString
+                    },
+                    "reprompt": {
+                        "outputSpeech": {
+                            "type": "PlainText",
+                            "text": respString + "  do need another one?",
+                        }
+                    },
+                    "shouldEndSession": false
+                }
+            };
+            console.log("==================================");
+            console.log(response.response);
+            context.succeed(response); 
+        });
+    }
+    
+    http.request(options, callback).end();
+
+ //   context.succeed(response); 
 }
 
