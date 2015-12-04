b9f528e4035c61a4d28f8ff4d2a61e3ca79979d8
alfredo aguirre <alfredodejesusaguirre@yahoo.com>
2015-12-03 21:31:04 -0600

    fix com error

--- AlexaService/Intent/SelectCar.cs
+++ AlexaService/Intent/SelectCar.cs
@@ -17,8 +17,8 @@ namespace AlexaService.Intent
             //Encountered in the URL fails or car combo is unknown. Not prompting the user for further information
             NegativeResponseTemplate.Add("I don't have that car in my records. Hm try again later.");
 
-            Response["yearStart"] = "styles[0].Year Start";
-            Response["yearLast"] = "styles[0].Year Last Proceed";
+            Response["yearStart"] = "styles[0].Years[0].year";
+            Response["yearLast"] = "styles[0].Year[::Last()].year";
             
             //Encountered if the user starts app without declaring both make and model
             ErrorSlotResponse["Make"] = "Who manufacturer of the car?";
@@ -28,7 +28,7 @@ namespace AlexaService.Intent
 
             FollowingQuestiestionMissingSlot["slot:Name"] = "pleace tell me ";
 
-            EdmundsUrlTemplate = https://api.edmunds.com/api/vehicle/v2/{slot:Make}/{slot:Model}?view=basic&fmt=json&api_key=67t7jtrnvz8wyzgfpwgcqa3y
+            EdmundsUrlTemplate = "https://api.edmunds.com/api/vehicle/v2/{slot:Make}/{slot:Model}?view=basic&fmt=json&api_key=67t7jtrnvz8wyzgfpwgcqa3y";
         }
     }
 }
