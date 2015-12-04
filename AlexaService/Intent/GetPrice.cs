eddd277ca8ca16f0fe0855d139560337491146ed
jamilavale <jamilatech101@gmail.com>
2015-12-03 20:13:35 -0600

    Updated responses

--- AlexaService/Intent/GetPrice.cs
+++ AlexaService/Intent/GetPrice.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,13 +11,19 @@ namespace AlexaService.Intent
         public GetPrice()
         {
             Name = "GetPrice";
-            PositiveResponseTemplate.Add("{slot:Name} have a starting price of {price}");
-            PositiveResponseTemplate.Add("{slot:Make} {slot:Name} have a starting price of {price}");
-
-            NegativeResponseTemplate.Add("I don't know");
+            PositiveResponseTemplate.Add("The {slot:Year} {slot:Make} {slot:Model} has a starting price of {price}");
+            PositiveResponseTemplate.Add("The {slot:Year} {slot:Make} {slot:Model} starts at {price} dollars");
+            
+            //Encountered in the URL fails or Price is unknown. Not prompting the user for further information
+            NegativeResponseTemplate.Add("Hmm. I can't seem to find the price at this time.");
 
             Response["price"] = "styles[0].price.baseMSRP";
 
+            //Encountered if the user starts app without declaring both make and model
+            ErrorSlotResponse["Make"] = "What's the make of the car?";
+            ErrorSlotResponse["Model"] = "What's the model of the car?";
+
+            
             EdmundsUrlTemplate = "https://api.edmunds.com/api/vehicle/v2/{slot:Make}/{slot:Name}/{slot:Year}/styles?view=full&fmt=json&api_key=67t7jtrnvz8wyzgfpwgcqa3y";
 
         }
