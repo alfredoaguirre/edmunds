b011d7d1ce48d1e7033eef3109e602dc1c59ed8e
jamilavale <jamilatech101@gmail.com>
2015-12-03 20:08:50 -0600

    Updated responses

--- AlexaService/Intent/GetMileage.cs
+++ AlexaService/Intent/GetMileage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,16 +11,19 @@ namespace AlexaService.Intent
         public GetMileage()
         {
             Name = "GetMileage";
-            PositiveResponseTemplate.Add("{slot:Name} have {highwayMpg}");
-            PositiveResponseTemplate.Add("{slot:Make} {slot:Name} have {highwayMpg}");
+            PositiveResponseTemplate.Add("The gas mileage of {slot:Year} {slot:Make} {slot:Model} is {cityMpg} in the city and {highwayMpg} on the highway");
+            PositiveResponseTemplate.Add("The city MPG of the {slot:Year} {slot:Make} {slot:Model} is {cityMpg} and the highway MPG is {highwayMpg}");
 
-            NegativeResponseTemplate.Add("I don't know");
+
+            //Encountered in the URL fails or MPG is unknown. Not prompting the user for further information
+            NegativeResponseTemplate.Add("I don't know at this time.");
 
             Response["highwayMpg"] = "styles[0].MPG.highway";
-            Response["cityyMpg"] = "styles[0].MPG.city";
-
-            ErrorSlotResponse["Name"] = "I don't know the name of th car";
-            ErrorSlotResponse["Make"] = "I don't know the name of th Make";
+            Response["cityMpg"] = "styles[0].MPG.city";
+            
+            //Encountered if the user starts app without declaring both make and model
+            ErrorSlotResponse["Make"] = "What's the make of the car?";
+            ErrorSlotResponse["Model"] = "What's the model of the car?";
 
             EdmundsUrlTemplate = "https://api.edmunds.com/api/vehicle/v2/{slot:Make}/{slot:Name}/{slot:Year}/styles?view=full&fmt=json&api_key=67t7jtrnvz8wyzgfpwgcqa3y";
 
