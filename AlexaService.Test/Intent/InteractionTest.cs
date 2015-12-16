be38c5052f05c5a953fcddf6ad13a2c211d1ae1f
jamilavale <jamilatech101@gmail.com>
2015-12-15 22:21:59 -0600

    add missing payload, and new interaction test

---
+++ AlexaService.Test/Intent/InteractionTest.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlexaService.Controllers;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
+using AlexaService.Intent;
+
+namespace AlexaService.Test.Intent
+{
+    [TestClass]
+    public class GetInteractionTest
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            IntentBase.UseResponseNumber = 0;
+            Cache.CacheManager.Clean();
+        }
+
+        
+        //Get Price - 2 other non mandatory parameters provided (Year, Model)
+        [TestMethod]
+        public void getCarPriceTest_2nonmandatory2()
+        {
+            AlexaService.Cache.CacheManager.AddSlots(new Dictionary<string, string>()
+                {
+                    {"Model", "Camry" },
+                    {"Year", "2002" },
+                }
+            );
+            var intent = new GetPrice();
+            //var edmundsURL = intent.GenEdmundsURL();
+            //var edmundsResponse = intent.GetEdmundsResponse();
+            var AlexaResponse=  intent.getAlexaResponse();
+            
+            Assert.AreEqual(AlexaResponse.response.outputSpeech.text, "What's the make of the car?");
+            
+            AlexaService.Cache.CacheManager.AddSlots(new Dictionary<string, string>()
+                {
+                    {"Make", "Toyota" }
+                }
+            );
+            
+            AlexaResponse = intent.getAlexaResponse();
+            
+            Assert.AreEqual(AlexaResponse.response.outputSpeech.text, "A new 2002 Toyota Camry has a starting price of 23700.");
+        }
+        
+    }
+}
